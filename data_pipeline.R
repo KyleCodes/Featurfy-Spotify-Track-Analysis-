f7c06913ef944ab1166fe9e900fd6f138d6e955c
Kyle Muldoon <kylemuldoon15@gmail.com>
2020-06-10 13:40:56 -0700

    reworked data pipeline into 3 sub functions for the steps of user input in the web app

--- data_pipeline.R
+++ data_pipeline.R
@@ -1,36 +1,42 @@
 source("spotify_wrapper.R")
 
-data_pipeline <- function() {
+
+retrieve_artists <- function(artist_query) {
+  
   token <- get_spotify_access_token()
-  
-  query <- "asap"
-  
+  query <- artist_query
   artist_results <- search_spotify(query, type = "artist", authorization = token)
+  return(artist_results)
+}
+
+
+
+retrieve_albums_by_artist <- function(artist_id) {
   
-  artist_id <- artist_results$id[1]
-  
+  token <- get_spotify_access_token()
   albums <- get_artist_albums(id = artist_id, include_groups = "album", authorization = token)
-  
-  album_IDs <- albums$id
-  
-  
+  return (albums)
+}
+
+retieve_songs_and_features <- function(album_IDs) {
+  token <- get_spotify_access_token()
   df = tibble()
   
   for (alb in album_IDs) {
-    
     albs_tracks <- get_album_tracks(id = alb, authorization = token)
-    
     track_ids <- albs_tracks$id
-    
     albs_track_features <- get_track_audio_features(track_ids)
-    
     df <- df %>% bind_rows(albs_track_features)
-    
-    
   }
   
   return (df)
 }
+
+
+
+
+
+
 
 
 
